37eb599a5199ca383ffd61c642683d61ad2b6de1
Mark Jason Fulguerinas <markjasonrfulguerinas@iskolarngbayan.pup.edu.ph>
2024-08-25 00:35:15 +0800

    LOADING

---
+++ main-script.js
@@ -0,0 +1,10 @@
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function(){
+  loader.classList.add("fade-out");  // Add fade-out class
+
+  // Wait for the fade-out animation to complete before hiding the element
+  loader.addEventListener("transitionend", function() {
+    loader.style.display = "none";
+  });
+});
