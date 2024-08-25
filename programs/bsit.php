be7239f34904c7913e0550ccec63195e24f6fa86
Mark Jason Fulguerinas <markjasonrfulguerinas@iskolarngbayan.pup.edu.ph>
2024-08-26 02:30:01 +0800

    programs-format

---
+++ programs/bsit.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Bachelor of Science in Information Technology (BSIT)</title>
+  <link rel="stylesheet" href="../assets/css/main-style.css" />
+  <link rel="icon" href="../assets/PUPSJ-LOGO.png" type="image/icon type" />
+</head>
+
+<body>
+  <header class="header-container"><?php include("../assets/static-html/header.html"); ?></header>
+
+  <section class="program">
+    <div class="header">
+      <img id="header-img" src="../assets/programs/BSIT/1.png" />
+      <div class="header-text">
+        <h id="header1">BACHELOR OF SCIENCE IN</h>
+        <h id="header2">INFORMATION TECHNOLOGY</h>
+      </div>
+    </div>
+
+    <div class="content-container">
+      <section class="prog-form pointers">
+        <h id="header">CONTENTS</h>
+        <a id="p1">Program Objectives</a>
+        <a id="p2">Areas Under Survey</a>
+      </section>
+
+      <div class="main-contents">
+        <section class="overview">
+          <h id="header">PROGRAM OVERVIEW</h>
+          <div class="thin-line"></div>
+          <h id="description">
+            The Bachelor of Science in Information Technology (BSIT) program is a four-year degree program which focuses
+            on the study of computer utilization and computer software to plan, install, customize, operate, manage,
+            administer and maintain information technology infrastructure. It likewise deals with the design and
+            development of computer-based information systems for real-world business solutions.
+            <p>
+              The program prepares students to become IT professionals with primary competencies in the areas of systems
+              analysis and design, applications development, database administration, network administration, and systems
+              implementation and maintenance.
+            </p>
+          </h>
+        </section>
+
+        <!-- <section class="program-imgs">
+          <div class="image-placeholder"></div>
+          <div class="image-placeholder"></div>
+          <div class="image-placeholder"></div>
+        </section> -->
+      </div>
+    </div>
+
+    <section class="objectives-bg">
+      <section class="objectives-container">
+        <section class="objectives-header">
+          <h id="header">PROGRAM OBJECTIVES</h>
+          <div class="thin-line"></div>
+        </section>
+
+        <ul>
+          <li>To introduce students to current technologies and tools while learning new methodologies that will lead to the development of better information systems.</li>
+          <li>To enable students to understand the different components of the information technology field, including hardware, software, communication, networking, research, peopleware, and management skills.</li>
+          <li>To demonstrate awareness of how to methodically and practically approach a variety of technological and managerial issues to ultimately improve business strategies and attain competitive advantage.</li>
+          <li>To inculcate to students the essential virtues and attitudes, as well as develop necessary knowledge and competency levels required of an information technology professional.</li>
+          <li>To train students to systematically analyze and evaluate organizational systems and processes in order to recommend software solutions that properly address the organization's needs and goals.</li>
+        </ul>
+      </section>
+    </section>
+
+
+    <a href="#"></a>
+
+    <section class="aus-container">
+      <div class="aus-header">AREAS UNDER SURVEY</div>
+      <div class="areas-img up">
+        <a href="#"><img src="../assets/programs/areas/AREA1.png" alt="vision, mission, goals and objectives"></a>
+        <a href="#"><img src="../assets/programs/areas/AREA2.png" alt="faculty"></a>
+        <a href="#"><img src="../assets/programs/areas/AREA3.png" alt="curriculum and instruction"></a>
+        <a href="#"><img src="../assets/programs/areas/AREA4.png" alt="support to students"></a>
+        <a href="#"><img src="../assets/programs/areas/AREA5.png" alt="research"></a>
+      </div>
+      <div class="areas-img down">
+        <a href="#"><img src="../assets/programs/areas/AREA6.png" alt="extension and community involvement"></a>
+        <a href="#"><img src="../assets/programs/areas/AREA7.png" alt="library"></a>
+        <a href="#"><img src="../assets/programs/areas/AREA8.png" alt="physical plant and facilities"></a>
+        <a href="#"><img src="../assets/programs/areas/AREA9.png" alt="laboratory"></a>
+        <a href="#"><img src="../assets/programs/areas/AREA10.png" alt="administration"></a>
+      </div>
+
+    </section>
+
+  </section>
+  <?php include("../assets/static-html/footer.html"); ?>
+
+  <script src="../main-script.js"></script>
+</body>
+
+</html>
