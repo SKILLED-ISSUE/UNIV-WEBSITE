66b86a31e9f061904f3716122666d88e0fe21bca
Mark Jason Fulguerinas <markjasonrfulguerinas@iskolarngbayan.pup.edu.ph>
2024-08-26 19:36:00 +0800

    Update bsit.php

--- programs/bsit.php
+++ programs/bsit.php
@@ -11,7 +11,7 @@
 
 <body>
   <header class="header-container">
-    <?php include(__DIR__ . '../assets/static-html/header.html'); ?>
+    <?php include(__DIR__ . 'assets/static-html/header.html'); ?>
   </header>
 
   <section class="program">
@@ -88,7 +88,7 @@
 
   </section>
 
-  <?php include(__DIR__ . '../assets/static-html/footer.html'); ?>
+  <?php include(__DIR__ . 'assets/static-html/footer.html'); ?>
 
   <script src="/main-script.js"></script>
 </body>
