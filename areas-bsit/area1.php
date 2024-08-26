e923e916118ca826d148e5bcc14e839957ac1541
Mark Jason Fulguerinas <markjasonrfulguerinas@iskolarngbayan.pup.edu.ph>
2024-08-27 01:59:25 +0800

    Update area1.php

--- areas-bsit/area1.php
+++ areas-bsit/area1.php
@@ -58,20 +58,53 @@
 
         <section class="collapsible-container">
             <button class="collapsible">
-                <div id="title">Parameter A</div>
-                <div id="desc">Statement of Vision, Mission, Goals, and Objectives</div>
+                <div class="title">Parameter A</div>
+                <div class="desc">Statement of Vision, Mission, Goals, and Objectives</div>
             </button>
             <div class="content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <ul>
+                    <li class="systems-inputs-processes"><strong>Systems - Inputs and Processes</strong>
+                        <ul>
+                            <li class="s1"><a href="#s1">S.1. The VMGO is available on bulletin boards, in catalogs/manuals, and in other forms of communication media</a></li>
+                        </ul>
+                    </li>
+                    <li class="implementation"><strong>Implementation</strong>
+                        <ul>
+                            <li class="i1"><a href="#i1">I.1. A system of dissemination and acceptability of the VMGO is enforced.</a></li>
+                            <li class="i2"><a href="#i2">I.2. The administrators/faculty attend in-service seminars and training on awareness and acceptability of the:</a>
+                                <ul>
+                                    <li class="i2-1">I.2.1. Vision and Mission of the Institution;</li>
+                                </ul>
+                            </li>
+                            <li class="i3"><a href="#i3">I.3. The formulation/review/revision of the VMGO is participated in by the following:</a>
+                                <ul>
+                                    <li class="i2-1">I.2.1. Vision and Mission of the Institution;</li>
+                                </ul>
+                            </li>
+                            <li class="i4"><a href="#i4">I.4. The faculty and staff perform their jobs/functions in consonance with the VMGO.</a></li>
+                            <li class="i5"><a href="#i5">I.5. The VMGO is widely disseminated to the different agencies, institutions, industry sector, and the community.</a></li>
+                        </ul>
+                    </li>
+                    <li class="outcomes"><strong>Outcome/s</strong>
+                        <ul>
+                            <li class="o1"><a href="#o1">O.1. There is full awareness and acceptance of the VMGO by the administrators, faculty, staff, students, and other stakeholders.</a></li>
+                            <li class="o2"><a href="#o2">O.2. There is congruency between actual educational practices and activities.</a></li>
+                            <li class="o3"><a href="#o3">O.3. The goals and objectives are being achieved</a></li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
             <button class="collapsible">
-                <div id="title">Parameter B</div>
-                <div id="desc">Dissemination and Acceptability</div>
+                <div class="title">Parameter B</div>
+                <div class="desc">Dissemination and Acceptability</div>
             </button>
             <div class="content">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             </div>
         </section>
+
+
+
 
     </section>
 
