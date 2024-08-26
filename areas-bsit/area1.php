cc3417287a2d073a5c5b7cae063986896b098ffd
Mark Jason Fulguerinas <markjasonrfulguerinas@iskolarngbayan.pup.edu.ph>
2024-08-27 02:25:56 +0800

    final-area

--- areas-bsit/area1.php
+++ areas-bsit/area1.php
@@ -63,33 +63,33 @@
             </button>
             <div class="content">
                 <ul>
-                    <li class="systems-inputs-processes"><strong>Systems - Inputs and Processes</strong>
+                    <li class="list-header"><strong>Systems - Inputs and Processes</strong>
                         <ul>
-                            <li class="s1"><a href="#s1">S.1. The VMGO is available on bulletin boards, in catalogs/manuals, and in other forms of communication media</a></li>
+                            <li class="sip">
+                                <a href="#"><strong>S.1.</strong> The VMGO is available on bulletin boards, in catalogs/manuals, and in other forms of communication media
+                                </a>
+                            </li>
                         </ul>
                     </li>
-                    <li class="implementation"><strong>Implementation</strong>
+                    <li class="list-header"><strong>Implementation</strong>
                         <ul>
-                            <li class="i1"><a href="#i1">I.1. A system of dissemination and acceptability of the VMGO is enforced.</a></li>
-                            <li class="i2"><a href="#i2">I.2. The administrators/faculty attend in-service seminars and training on awareness and acceptability of the:</a>
+                            <li class="imp">
+                                <a href="#">
+                                    <strong>I.1.</strong> The administrators/faculty attend in-service seminars and training on awareness and acceptability of the:
+                                </a>
                                 <ul>
-                                    <li class="i2-1">I.2.1. Vision and Mission of the Institution;</li>
+                                    <li>I.2.1. Vision and Mission of the Institution;</li>
                                 </ul>
                             </li>
-                            <li class="i3"><a href="#i3">I.3. The formulation/review/revision of the VMGO is participated in by the following:</a>
-                                <ul>
-                                    <li class="i2-1">I.2.1. Vision and Mission of the Institution;</li>
-                                </ul>
-                            </li>
-                            <li class="i4"><a href="#i4">I.4. The faculty and staff perform their jobs/functions in consonance with the VMGO.</a></li>
-                            <li class="i5"><a href="#i5">I.5. The VMGO is widely disseminated to the different agencies, institutions, industry sector, and the community.</a></li>
                         </ul>
                     </li>
-                    <li class="outcomes"><strong>Outcome/s</strong>
+                    <li class="list-header"><strong>Outcome/s</strong>
                         <ul>
-                            <li class="o1"><a href="#o1">O.1. There is full awareness and acceptance of the VMGO by the administrators, faculty, staff, students, and other stakeholders.</a></li>
-                            <li class="o2"><a href="#o2">O.2. There is congruency between actual educational practices and activities.</a></li>
-                            <li class="o3"><a href="#o3">O.3. The goals and objectives are being achieved</a></li>
+                            <li class="out">
+                                <a href="#">
+                                    <strong>O.1.</strong> There is full awareness and acceptance of the VMGO by the administrators, faculty, staff, students, and other stakeholders.
+                                </a>
+                            </li>
                         </ul>
                     </li>
                 </ul>
