bc9204ec84c22a3756a06042dd51113fc68066e6
Mark Jason Fulguerinas <markjasonrfulguerinas@iskolarngbayan.pup.edu.ph>
2024-08-27 02:39:45 +0800

    deployable

---
+++ programs/program-template.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Bachelor of Science in Information Technology (BSIT)</title>
+  <link rel="stylesheet" href="../assets/css/main-style.css" />
+  <link rel="icon" href="../assets/PUPSJ-LOGO.png" type="image/icon type" />
+</head>
+
+<body>
+  <header class="header-container">
+    <?php include('../assets/static-html/header.html'); ?>
+  </header>
+
+  <section class="program">
+    <div class="header">
+      <img id="header-img" src="../assets/programs/BSIT/1.png" />
+      <div class="header-text">
+        <h id="header1">Program</h>
+        <h id="header2">Title</h>
+      </div>
+    </div>
+
+    <div class="content-container">
+      <section class="pointers">
+        <h id="header">CONTENTS</h>
+        <a href="#c1" class='scroll-link'>Program Objectives</a>
+        <a href="#c2" class='scroll-link'>Areas Under Survey</a>
+      </section>
+
+      <div class="main-contents">
+        <section class="overview">
+          <h id="header">PROGRAM OVERVIEW</h>
+          <div class="thin-line"></div>
+          <h id="description">description
+          </h>
+        </section>
+      </div>
+    </div>
+
+    <img id="banner" src="../assets/programs/BSIT/IT-BANNER.png" alt="program-banner">
+
+    <section class="objectives-bg anchor-offset" id="c1">
+      <section class="objectives-container">
+        <section class="objectives-header">
+          <h id="header">PROGRAM OBJECTIVES</h>
+          <div class="thin-line"></div>
+        </section>
+
+        <ul>
+          <li>description.</li>
+          <li>description.</li>
+          <li>description.</li>
+          <li>description.</li>
+          <li>description.</li>
+        </ul>
+      </section>
+    </section>
+
+    <section class="aus-container anchor-offset" id="c2">
+      <div class="aus-header">AREAS UNDER SURVEY</div>
+      <div class="areas-img up">
+        <a href="../areas-bsit/area1.php"><img src="../assets/programs/areas/AREA1.png" alt="vision, mission, goals and objectives"></a>
+        <a href="../areas-bsit/area2.php"><img src="../assets/programs/areas/AREA2.png" alt="faculty"></a>
+        <a href="../areas-bsit/area3.php"><img src="../assets/programs/areas/AREA3.png" alt="curriculum and instruction"></a>
+        <a href="../areas-bsit/area4.php"><img src="../assets/programs/areas/AREA4.png" alt="support to students"></a>
+        <a href="../areas-bsit/area5.php"><img src="../assets/programs/areas/AREA5.png" alt="research"></a>
+      </div>
+      <div class="areas-img down">
+        <a href="../areas-bsit/area6.php"><img src="../assets/programs/areas/AREA6.png" alt="extension and community involvement"></a>
+        <a href="../areas-bsit/area7.php"><img src="../assets/programs/areas/AREA7.png" alt="library"></a>
+        <a href="../areas-bsit/area8.php"><img src="../assets/programs/areas/AREA8.png" alt="physical plant and facilities"></a>
+        <a href="../areas-bsit/area9.php"><img src="../assets/programs/areas/AREA9.png" alt="laboratory"></a>
+        <a href="../areas-bsit/area10.php"><img src="../assets/programs/areas/AREA10.png" alt="administration"></a>
+      </div>
+
+    </section>
+
+  </section>
+
+  <?php include('../assets/static-html/footer.html'); ?>
+
+  <script src="../main-script.js"></script>
+</body>
+
+</html>
