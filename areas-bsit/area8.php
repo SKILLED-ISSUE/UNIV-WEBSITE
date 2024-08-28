bbba1899c436fea44b82a5c7672ab89f37725d20
AgustinUno <justinelloydgbautista@gmail.com>
2024-08-28 17:06:58 +0800

    area8-done

--- areas-bsit/area8.php
+++ areas-bsit/area8.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>BSIT - Area 1</title>
+    <title>BSIT - Area 8</title>
     <link rel="stylesheet" href="../assets/css/main-style.css" />
     <link rel="icon" href="../assets/PUPSJ-LOGO.png" type="image/icon type" />
 </head>
@@ -18,18 +18,16 @@
         <div class="header">
             <img id="header-img" src="../assets/programs/BSIT/1.png" />
             <div class="header-text" class="fade-in">
-                <h id="header1" class="fade-in">AREA 1</h>
-                <h id="header2" class="fade-in">Vision, Mission, Goals and Objectives</h>
+                <h id="header1" class="fade-in">AREA 8</h>
+                <h id="header2" class="fade-in">Physical Plant and Facilities</h>
             </div>
         </div>
 
         <div id="description" class="fade-in">
-            The area of Vision, Mission, Goals, and Objectives is the most fundamental of
-            all the (10) areas to be surveyed. Everything in the Institution is justified
-            only to the extent that it realizes its vision and mission. It is essential
-            therefore, for the Institution to formulate the vision and mission which should
-            be the bases of all its operations. The Institution is judged by the degree to
-            which these are attained, not in comparison with others.
+            The quality and adequacy of the physical plant and facilities of a learning institution determine to a large
+            measure the successful implementation of its curricular programs. In a broad sense, physical plant and
+            facilities include school campus, buildings and other physical infrastructures, equipment and services that
+            complement institutional and program effectiveness.
         </div>
 
         <div class="line-container">
@@ -37,7 +35,8 @@
         </div>
 
         <div class="img-thmb" class="fade-in">
-            <a href="https://drive.google.com/file/d/1R2bHJ23zdV33Zx5wKnN-Viz8kXZqhek_/view?usp=sharing" target="_blank">
+            <a href="https://drive.google.com/file/d/1R2bHJ23zdV33Zx5wKnN-Viz8kXZqhek_/view?usp=sharing"
+                target="_blank">
                 <img src="PPPA1.png" alt="program performance profile">
                 <div class="overlay">
                     <div class="text">Click here</div>
@@ -57,82 +56,300 @@
         </div>
 
         <section class="collapsible-container">
+
+            <!-- PARAMETER A -->
+
             <button class="collapsible">
                 <div class="title">Parameter A</div>
-                <div class="desc">Statement of Vision, Mission, Goals, and Objectives</div>
+                <div class="desc">Campus</div>
             </button>
             <div class="content">
                 <ul>
                     <li class="list-header"><strong>Systems - Inputs and Processes</strong>
                         <ul>
                             <li class="sip">
-                                <a href="#"><strong>S.1.</strong> The VMGO is available on bulletin boards, in catalogs/manuals, and in other forms of communication media
-                                </a>
+                                <a href="#">To access the evidences for S.1 to S.6, click here.</a>
                             </li>
                         </ul>
                     </li>
                     <li class="list-header"><strong>Implementation</strong>
                         <ul>
                             <li class="imp">
-                                <a href="#">
-                                    <strong>I.1.</strong> The administrators/faculty attend in-service seminars and training on awareness and acceptability of the:
-                                </a>
-                                <ul>
-                                    <li>I.2.1. Vision and Mission of the Institution;</li>
-                                </ul>
+                                <a href="#">To access the evidences for I.1 to I.6, click here.</a>
                             </li>
                         </ul>
                     </li>
                     <li class="list-header"><strong>Outcome/s</strong>
                         <ul>
                             <li class="out">
-                                <a href="#">
-                                    <strong>O.1.</strong> There is full awareness and acceptance of the VMGO by the administrators, faculty, staff, students, and other stakeholders.
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            <button class="collapsible">
-                <div class="title">Parameter B</div>
-                <div class="desc">Dissemination and Acceptability</div>
-            </button>
-            <div class="content">
-                <ul>
-                    <li class="list-header"><strong>Systems - Inputs and Processes</strong>
-                        <ul>
-                            <li class="sip">
-                                <a href="#"><strong>S.1.</strong> The VMGO is available on bulletin boards, in catalogs/manuals, and in other forms of communication media
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="list-header"><strong>Implementation</strong>
-                        <ul>
-                            <li class="imp">
-                                <a href="#">
-                                    <strong>I.1.</strong> The administrators/faculty attend in-service seminars and training on awareness and acceptability of the:
-                                </a>
-                                <ul>
-                                    <li>I.2.1. Vision and Mission of the Institution;</li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="list-header"><strong>Outcome/s</strong>
-                        <ul>
-                            <li class="out">
-                                <a href="#">
-                                    <strong>O.1.</strong> There is full awareness and acceptance of the VMGO by the administrators, faculty, staff, students, and other stakeholders.
-                                </a>
+                                <a href="#">To access the evidences for O.1 to O.4, click here.</a>
                             </li>
                         </ul>
                     </li>
                 </ul>
             </div>
 
-            
+            <!-- PARAMETER B -->
+
+            <button class="collapsible">
+                <div class="title">Parameter B</div>
+                <div class="desc">Buildings</div>
+            </button>
+            <div class="content">
+                <ul>
+                    <li class="list-header"><strong>Systems - Inputs and Processes</strong>
+                        <ul>
+                            <li class="sip">
+                                <a href="#">To access the evidences for S.1 to S.14, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Implementation</strong>
+                        <ul>
+                            <li class="imp">
+                                <a href="#">To access the evidences for I.1 to I.19, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Outcome/s</strong>
+                        <ul>
+                            <li class="out">
+                                <a href="#">To access the evidences for O.1, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- PARAMETER C -->
+
+            <button class="collapsible">
+                <div class="title">Parameter C</div>
+                <div class="desc">Classrooms</div>
+            </button>
+            <div class="content">
+                <ul>
+                    <li class="list-header"><strong>Systems - Inputs and Processes</strong>
+                        <ul>
+                            <li class="sip">
+                                <a href="#">To access the evidences for S.1 to S.4, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Implementation</strong>
+                        <ul>
+                            <li class="imp">
+                                <a href="#">To access the evidences for I.1 to I.3, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Outcome/s</strong>
+                        <ul>
+                            <li class="out">
+                                <a href="#">To access the evidences for O.1, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- PARAMETER D -->
+
+            <button class="collapsible">
+                <div class="title">Parameter D</div>
+                <div class="desc">Offices and Staff Rooms</div>
+            </button>
+            <div class="content">
+                <ul>
+                    <li class="list-header"><strong>Systems - Inputs and Processes</strong>
+                        <ul>
+                            <li class="sip">
+                                <a href="#">To access the evidences for S.1 to S.8, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Implementation</strong>
+                        <ul>
+                            <li class="imp">
+                                <a href="#">To access the evidences for I.1 to I.2, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Outcome/s</strong>
+                        <ul>
+                            <li class="out">
+                                <a href="#">To access the evidences for O.1, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- PARAMETER E -->
+
+            <button class="collapsible">
+                <div class="title">Parameter E</div>
+                <div class="desc">Assembly and Athletic Facilities
+                </div>
+            </button>
+            <div class="content">
+                <ul>
+                    <li class="list-header"><strong>Systems - Inputs and Processes</strong>
+                        <ul>
+                            <li class="sip">
+                                <a href="#">To access the evidences for S.1 to S.5, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Implementation</strong>
+                        <ul>
+                            <li class="imp">
+                                <a href="#">To access the evidences for I.1 to I.4, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Outcome/s</strong>
+                        <ul>
+                            <li class="out">
+                                <a href="#">To access the evidences for O.1, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- PARAMETER F -->
+
+            <button class="collapsible">
+                <div class="title">Parameter F</div>
+                <div class="desc">Medical and Dental Clinic</div>
+            </button>
+            <div class="content">
+                <ul>
+                    <li class="list-header"><strong>Systems - Inputs and Processes</strong>
+                        <ul>
+                            <li class="sip">
+                                <a href="#">To access the evidences for S.1 to S.10, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Implementation</strong>
+                        <ul>
+                            <li class="imp">
+                                <a href="#">To access the evidences for I.1 to I.3, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Outcome/s</strong>
+                        <ul>
+                            <li class="out">
+                                <a href="#">To access the evidences for O.1, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+
+            <!-- PARAMETER G -->
+
+            <button class="collapsible">
+                <div class="title">Parameter G</div>
+                <div class="desc">Student Center</div>
+            </button>
+            <div class="content">
+                <ul>
+                    <li class="list-header"><strong>Systems - Inputs and Processes</strong>
+                        <ul>
+                            <li class="sip">
+                                <a href="#">To access the evidences for S.1 to S.8, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Implementation</strong>
+                        <ul>
+                            <li class="imp">
+                                <a href="#">To access the evidences for I.1 to I.2, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Outcome/s</strong>
+                        <ul>
+                            <li class="out">
+                                <a href="#">To access the evidences for O.1, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- PARAMETER H -->
+
+            <button class="collapsible">
+                <div class="title">Parameter H</div>
+                <div class="desc">Food Services/Canteen/Cafeteria
+                </div>
+            </button>
+            <div class="content">
+                <ul>
+                    <li class="list-header"><strong>Systems - Inputs and Processes</strong>
+                        <ul>
+                            <li class="sip">
+                                <a href="#">To access the evidences for S.1 to S.3, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Implementation</strong>
+                        <ul>
+                            <li class="imp">
+                                <a href="#">To access the evidences for I.1 to I.5, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Outcome/s</strong>
+                        <ul>
+                            <li class="out">
+                                <a href="#">To access the evidences for O.1 to O.2, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- PARAMETER I -->
+
+            <button class="collapsible">
+                <div class="title">Parameter I</div>
+                <div class="desc">Accreditation Center</div>
+            </button>
+            <div class="content">
+                <ul>
+                    <li class="list-header"><strong>Systems - Inputs and Processes</strong>
+                        <ul>
+                            <li class="sip">
+                                <a href="#">To access the evidences for S.1 to S.2, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Implementation</strong>
+                        <ul>
+                            <li class="imp">
+                                <a href="#">To access the evidences for I.1 to I.3, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Outcome/s</strong>
+                        <ul>
+                            <li class="out">
+                                <a href="#">To access the evidences for O.1, click here.</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+
+
         </section>
 
 
