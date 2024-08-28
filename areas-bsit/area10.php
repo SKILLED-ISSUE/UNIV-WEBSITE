81587599709ef161eed1814dfe3078a9c0c31a30
Regie <sanjuanregie@gmail.com>
2024-08-28 08:33:54 +0800

    area10.1

--- areas-bsit/area10.php
+++ areas-bsit/area10.php
@@ -4,32 +4,28 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>BSIT - Area 1</title>
+    <title>BSIT - Area 10</title>
     <link rel="stylesheet" href="../assets/css/main-style.css" />
     <link rel="icon" href="../assets/PUPSJ-LOGO.png" type="image/icon type" />
 </head>
 
 <body>
     <header class="header-container">
-        <?php include('../assets/static-html/header.html'); ?>
+        <?php include __DIR__ . '/../assets/static-html/header.html'; ?>
     </header>
 
     <section class="area program">
         <div class="header">
             <img id="header-img" src="../assets/programs/BSIT/1.png" />
             <div class="header-text" class="fade-in">
-                <h id="header1" class="fade-in">AREA 1</h>
-                <h id="header2" class="fade-in">Vision, Mission, Goals and Objectives</h>
+                <h id="header1" class="fade-in">AREA 10</h>
+                <h id="header2" class="fade-in">Administration</h>
             </div>
         </div>
 
         <div id="description" class="fade-in">
-            The area of Vision, Mission, Goals, and Objectives is the most fundamental of
-            all the (10) areas to be surveyed. Everything in the Institution is justified
-            only to the extent that it realizes its vision and mission. It is essential
-            therefore, for the Institution to formulate the vision and mission which should
-            be the bases of all its operations. The Institution is judged by the degree to
-            which these are attained, not in comparison with others.
+        The administration is the engine of the institution in the attainment of its vision, mission, goals and objectives. 
+        It is concerned with the general affairs of the institution and its organizational performance. Thus, the administration adopts institutional processes and ensures that said processes are satisfactorily implemented.
         </div>
 
         <div class="line-container">
@@ -59,7 +55,242 @@
         <section class="collapsible-container">
             <button class="collapsible">
                 <div class="title">Parameter A</div>
-                <div class="desc">Statement of Vision, Mission, Goals, and Objectives</div>
+                <div class="desc">Organization</div>
+            </button>
+            <div class="content">
+                <ul>
+                    <li class="list-header"><strong>Systems - Inputs and Processes</strong>
+                        <ul>
+                            <li class="sip">
+                                <a href="#"><strong>S.1.</strong> The institution has an organizational structure approved by the Board of Regents/Trustees (BOR/BOT).
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#"><strong>S.2.</strong> Every office/unit in the organizational structure has functions approved by the BOR/BOT.</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Implementation</strong>
+                        <ul>
+                            <li class="imp">
+                                <a href="#">
+                                    <strong>I.1.</strong> The institution is subdivided into administrative units in accordance with the organizational structure.
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#"><strong>I.2.</strong> The functions, duties, and responsibilities of administrative staff in each unit/office are carried out.</a>
+                            </li>
+                            <li>
+                                <a href="#"><strong>I.3.</strong> The Board of Regents/Trustees is supportive with the growth and development of the institution.</a>
+                            </li>
+                            <li>
+                                <a href="#"><strong>I.4.</strong> The Academic and Administrative Councils exercise their powers and perform their functions.</a>
+                            </li>
+                            <li>
+                                <a href="#"><strong>I.5.</strong> The flow of communication among and within units/departments is observed.</a>
+                            </li>
+                            
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Outcome/s</strong>
+                        <ul>
+                            <li class="out">
+                                <a href="#">
+                                    <strong>O.1.</strong> The institution has a well-designed and functional organizational structure.
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <button class="collapsible">
+                <div class="title">Parameter B</div>
+                <div class="desc">Dissemination and Acceptability</div>
+            </button>
+            <div class="content">
+                <ul>
+                    <li class="list-header"><strong>Systems - Inputs and Processes</strong>
+                        <ul>
+                            <li class="sip">
+                                <a href="#"><strong>S.1.</strong> The VMGO is available on bulletin boards, in catalogs/manuals, and in other forms of communication media
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Implementation</strong>
+                        <ul>
+                            <li class="imp">
+                                <a href="#">
+                                    <strong>I.1.</strong> The administrators/faculty attend in-service seminars and training on awareness and acceptability of the:
+                                </a>
+                                <ul>
+                                    <li>I.2.1. Vision and Mission of the Institution;</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Outcome/s</strong>
+                        <ul>
+                            <li class="out">
+                                <a href="#">
+                                    <strong>O.1.</strong> There is full awareness and acceptance of the VMGO by the administrators, faculty, staff, students, and other stakeholders.
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <button class="collapsible">
+                <div class="title">Parameter B</div>
+                <div class="desc">Dissemination and Acceptability</div>
+            </button>
+            <div class="content">
+                <ul>
+                    <li class="list-header"><strong>Systems - Inputs and Processes</strong>
+                        <ul>
+                            <li class="sip">
+                                <a href="#"><strong>S.1.</strong> The VMGO is available on bulletin boards, in catalogs/manuals, and in other forms of communication media
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Implementation</strong>
+                        <ul>
+                            <li class="imp">
+                                <a href="#">
+                                    <strong>I.1.</strong> The administrators/faculty attend in-service seminars and training on awareness and acceptability of the:
+                                </a>
+                                <ul>
+                                    <li>I.2.1. Vision and Mission of the Institution;</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Outcome/s</strong>
+                        <ul>
+                            <li class="out">
+                                <a href="#">
+                                    <strong>O.1.</strong> There is full awareness and acceptance of the VMGO by the administrators, faculty, staff, students, and other stakeholders.
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <button class="collapsible">
+                <div class="title">Parameter B</div>
+                <div class="desc">Dissemination and Acceptability</div>
+            </button>
+            <div class="content">
+                <ul>
+                    <li class="list-header"><strong>Systems - Inputs and Processes</strong>
+                        <ul>
+                            <li class="sip">
+                                <a href="#"><strong>S.1.</strong> The VMGO is available on bulletin boards, in catalogs/manuals, and in other forms of communication media
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Implementation</strong>
+                        <ul>
+                            <li class="imp">
+                                <a href="#">
+                                    <strong>I.1.</strong> The administrators/faculty attend in-service seminars and training on awareness and acceptability of the:
+                                </a>
+                                <ul>
+                                    <li>I.2.1. Vision and Mission of the Institution;</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Outcome/s</strong>
+                        <ul>
+                            <li class="out">
+                                <a href="#">
+                                    <strong>O.1.</strong> There is full awareness and acceptance of the VMGO by the administrators, faculty, staff, students, and other stakeholders.
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <button class="collapsible">
+                <div class="title">Parameter B</div>
+                <div class="desc">Dissemination and Acceptability</div>
+            </button>
+            <div class="content">
+                <ul>
+                    <li class="list-header"><strong>Systems - Inputs and Processes</strong>
+                        <ul>
+                            <li class="sip">
+                                <a href="#"><strong>S.1.</strong> The VMGO is available on bulletin boards, in catalogs/manuals, and in other forms of communication media
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Implementation</strong>
+                        <ul>
+                            <li class="imp">
+                                <a href="#">
+                                    <strong>I.1.</strong> The administrators/faculty attend in-service seminars and training on awareness and acceptability of the:
+                                </a>
+                                <ul>
+                                    <li>I.2.1. Vision and Mission of the Institution;</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Outcome/s</strong>
+                        <ul>
+                            <li class="out">
+                                <a href="#">
+                                    <strong>O.1.</strong> There is full awareness and acceptance of the VMGO by the administrators, faculty, staff, students, and other stakeholders.
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <button class="collapsible">
+                <div class="title">Parameter B</div>
+                <div class="desc">Dissemination and Acceptability</div>
+            </button>
+            <div class="content">
+                <ul>
+                    <li class="list-header"><strong>Systems - Inputs and Processes</strong>
+                        <ul>
+                            <li class="sip">
+                                <a href="#"><strong>S.1.</strong> The VMGO is available on bulletin boards, in catalogs/manuals, and in other forms of communication media
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Implementation</strong>
+                        <ul>
+                            <li class="imp">
+                                <a href="#">
+                                    <strong>I.1.</strong> The administrators/faculty attend in-service seminars and training on awareness and acceptability of the:
+                                </a>
+                                <ul>
+                                    <li>I.2.1. Vision and Mission of the Institution;</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Outcome/s</strong>
+                        <ul>
+                            <li class="out">
+                                <a href="#">
+                                    <strong>O.1.</strong> There is full awareness and acceptance of the VMGO by the administrators, faculty, staff, students, and other stakeholders.
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <button class="collapsible">
+                <div class="title">Parameter B</div>
+                <div class="desc">Dissemination and Acceptability</div>
             </button>
             <div class="content">
                 <ul>
@@ -142,7 +373,7 @@
 
 
 
-    <?php include('../assets/static-html/footer.html'); ?>
+    <?php include __DIR__ . '/../assets/static-html/footer.html'; ?>
 
     <script src="../main-script.js"></script>
 </body>
