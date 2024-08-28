9cf78f6b4c9bea0865af47ba6210222c8646d7bf
Shiroe_11011 <ilardecharles@gmail.com>
2024-08-28 22:01:36 +0800

    Area 6 (Complete)

--- areas-bsit/area6.php
+++ areas-bsit/area6.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>BSIT - Area 1</title>
+    <title>BSIT - Area 6</title>
     <link rel="stylesheet" href="../assets/css/main-style.css" />
     <link rel="icon" href="../assets/PUPSJ-LOGO.png" type="image/icon type" />
 </head>
@@ -19,17 +19,26 @@
             <img id="header-img" src="../assets/programs/BSIT/1.png" />
             <div class="header-text" class="fade-in">
                 <h id="header1" class="fade-in">AREA 1</h>
-                <h id="header2" class="fade-in">Vision, Mission, Goals and Objectives</h>
+                <h id="header2" class="fade-in">EXTENSION AND COMMUNITY INVOLVEMENT</h>
             </div>
         </div>
 
         <div id="description" class="fade-in">
-            The area of Vision, Mission, Goals, and Objectives is the most fundamental of
-            all the (10) areas to be surveyed. Everything in the Institution is justified
-            only to the extent that it realizes its vision and mission. It is essential
-            therefore, for the Institution to formulate the vision and mission which should
-            be the bases of all its operations. The Institution is judged by the degree to
-            which these are attained, not in comparison with others.
+          The extension function makes the institution's presence felt in the community. 
+          It involves the application of existing and new knowledge and technology and 
+          those generated in the Institution to improve the quality of life of the people. 
+          Through the extension program, people are empowered with appropriate knowledge, 
+          attitudes and skills. Thus, extension services cater to various aspects of the 
+          community life, e.g., economic growth, promotion of health, environmental management, 
+          and social transformation.
+        <br></br>
+          The Institution plans and implements an extension program that is need and client-based. 
+          This program should have a budgetary support and other resource allocation. The faculty 
+          members may serve as experts, consultants, organizers, facilitators, coordinators, service providers, 
+          and change agents in the community as forms of extension and community involvement.
+        <br></br>
+          Careful planning and coordination with other community outreach agencies should be 
+          considered to avoid duplication of services offered to the clientele.  
         </div>
 
         <div class="line-container">
@@ -59,14 +68,14 @@
         <section class="collapsible-container">
             <button class="collapsible">
                 <div class="title">Parameter A</div>
-                <div class="desc">Statement of Vision, Mission, Goals, and Objectives</div>
+                <div class="desc">Priorities and Relevance</div>
             </button>
             <div class="content">
                 <ul>
                     <li class="list-header"><strong>Systems - Inputs and Processes</strong>
                         <ul>
                             <li class="sip">
-                                <a href="#"><strong>S.1.</strong> The VMGO is available on bulletin boards, in catalogs/manuals, and in other forms of communication media
+                                <a href="#"><strong>S.1-S.5.</strong> Click here to access the evidences for S.1 to S.5.
                                 </a>
                             </li>
                         </ul>
@@ -75,11 +84,8 @@
                         <ul>
                             <li class="imp">
                                 <a href="#">
-                                    <strong>I.1.</strong> The administrators/faculty attend in-service seminars and training on awareness and acceptability of the:
+                                    <strong>I.1-I.4.</strong> Click here to access the evidences for I.1 to I.4.
                                 </a>
-                                <ul>
-                                    <li>I.2.1. Vision and Mission of the Institution;</li>
-                                </ul>
                             </li>
                         </ul>
                     </li>
@@ -87,7 +93,7 @@
                         <ul>
                             <li class="out">
                                 <a href="#">
-                                    <strong>O.1.</strong> There is full awareness and acceptance of the VMGO by the administrators, faculty, staff, students, and other stakeholders.
+                                    <strong>O.1.</strong> Click here to access the evidences for O.1.
                                 </a>
                             </li>
                         </ul>
@@ -96,14 +102,14 @@
             </div>
             <button class="collapsible">
                 <div class="title">Parameter B</div>
-                <div class="desc">Dissemination and Acceptability</div>
+                <div class="desc">Planning, Implementation, Monitoring, and Evaluation</div>
             </button>
             <div class="content">
                 <ul>
                     <li class="list-header"><strong>Systems - Inputs and Processes</strong>
                         <ul>
                             <li class="sip">
-                                <a href="#"><strong>S.1.</strong> The VMGO is available on bulletin boards, in catalogs/manuals, and in other forms of communication media
+                                <a href="#"><strong>S.1-S.2.</strong> Click here to access the evidences for S.1 to S.2.
                                 </a>
                             </li>
                         </ul>
@@ -112,11 +118,8 @@
                         <ul>
                             <li class="imp">
                                 <a href="#">
-                                    <strong>I.1.</strong> The administrators/faculty attend in-service seminars and training on awareness and acceptability of the:
+                                    <strong>I.1-I.10.</strong> Click here to access the evidences for I.1 to I.10.
                                 </a>
-                                <ul>
-                                    <li>I.2.1. Vision and Mission of the Institution;</li>
-                                </ul>
                             </li>
                         </ul>
                     </li>
@@ -124,24 +127,83 @@
                         <ul>
                             <li class="out">
                                 <a href="#">
-                                    <strong>O.1.</strong> There is full awareness and acceptance of the VMGO by the administrators, faculty, staff, students, and other stakeholders.
+                                    <strong>O.1-O.2.</strong> Click here to access the evidences for O.1 to O.2.
                                 </a>
                             </li>
                         </ul>
                     </li>
                 </ul>
             </div>
-
-            
+            <button class="collapsible">
+                <div class="title">Parameter C</div>
+                <div class="desc">Funding and other Resources</div>
+            </button>
+            <div class="content">
+                <ul>
+                    <li class="list-header"><strong>Systems - Inputs and Processes</strong>
+                        <ul>
+                            <li class="sip">
+                                <a href="#"><strong>S.1-S.2.</strong> Click here to access the evidences for S.1 to S.2.
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Implementation</strong>
+                        <ul>
+                            <li class="imp">
+                                <a href="#">
+                                    <strong>I.1-I.3.</strong> Click here to access the evidences for I.1 to I.3.
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Outcome/s</strong>
+                        <ul>
+                            <li class="out">
+                                <a href="#">
+                                    <strong>O.1.</strong> Click here to access the evidences for O.1.
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <button class="collapsible">
+                <div class="title">Parameter D</div>
+                <div class="desc">Community Involvement and Participation in the Institution's Activity</div>
+            </button>
+            <div class="content">
+                <ul>
+                    <li class="list-header"><strong>Systems - Inputs and Processes</strong>
+                        <ul>
+                            <li class="sip">
+                                <a href="#"><strong>S.1.</strong> Click here to access the evidences for S.1.
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Implementation</strong>
+                        <ul>
+                            <li class="imp">
+                                <a href="#">
+                                    <strong>I.1-I.2.</strong> Click here to access the evidences for I.1 to I.2.
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="list-header"><strong>Outcome/s</strong>
+                        <ul>
+                            <li class="out">
+                                <a href="#">
+                                    <strong>O.1.</strong> Click here to access the evidences for O.1.
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </section>
-
-
-
-
     </section>
-
-
-
     <?php include('../assets/static-html/footer.html'); ?>
 
     <script src="../main-script.js"></script>
