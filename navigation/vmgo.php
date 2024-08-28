2f9d116f4faa15d9c8f80125d6b6f44e5193a172
AgustinUno <justinelloydgbautista@gmail.com>
2024-08-28 22:32:41 +0800

    partial vmgo

--- navigation/vmgo.php
+++ navigation/vmgo.php
@@ -19,31 +19,51 @@
         <div class="header">
             <img id="header-img" src="../assets/programs/BSIT/1.png" />
             <div class="header-text" class="fade-in">
-               
+
                 <h id="header2" class="fade-in">Vision and Mission</h>
             </div>
         </div>
-       
-        <div id="description" class="fade-in">
-        <p id="quote-title">From an Epistemic Community Towards a National Polytechnic University of the Philippines</p>
-        The stellar accomplishments of the PUP San Juan Branch is ably founded on the University's vision: first as an epistemic community towards a National Polytechnic University of the Philippines. In an effort to get there, the Program is also strongly supporting the mission and development agenda of the University Leadership. What started as an Eight-Point Development Plan under former University President Dr. Emanuel De Guzman was expanded and fortified by the 10 Pillar Reform Agenda of current President, Dr. Manuel Muhi. 
-        
-        <p id="note"><i>The video below developed by the PUP Communication Management Office (CMO) briefly describes President Muhi's vision for the University.</i></p>
-        </div>
-       
-        <iframe width="1351" height="518" src="https://www.youtube.com/embed/i7qQVvI8fOI" title="PUP Vision, Mission, and 10 Pillars Reform Agenda" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
-        <div class="line-container">
+        <div id="description" class="fade-in">
+            <p id="quote-title">From an Epistemic Community Towards a National Polytechnic University of the Philippines
+            </p>
+            The stellar accomplishments of the PUP San Juan Branch is ably founded on the University's vision: first as
+            an epistemic community towards a National Polytechnic University of the Philippines. In an effort to get
+            there, the Program is also strongly supporting the mission and development agenda of the University
+            Leadership. What started as an Eight-Point Development Plan under former University President Dr. Emanuel De
+            Guzman was expanded and fortified by the 10 Pillar Reform Agenda of current President, Dr. Manuel Muhi.
+
+            <p id="note"><i>The video below developed by the PUP Communication Management Office (CMO) briefly describes
+                    President Muhi's vision for the University.</i></p>
+        </div>
+
+        <iframe width="1351" height="518" src="https://www.youtube.com/embed/i7qQVvI8fOI"
+            title="PUP Vision, Mission, and 10 Pillars Reform Agenda" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+        <!-- <div class="line-container">
             <div id="thin-line" class="one"></div>
         </div>
 
         <div class="line-container">
             <div class="two" id="thin-line"></div>
+        </div> -->
+
+        <div class="card">
+            <div class="card-header">
+                Our Mission
+            </div>
+            <div class="card-body">
+                <p>PUP: The National Polytechnic University</p>
+                <p>PUP: Pambansang Politeknikong Unibersidad</p>
+            </div>
         </div>
 
-       
+
+
     </section>
-    
+
     <?php include('../assets/static-html/footer.html'); ?>
 
     <script src="../main-script.js"></script>
