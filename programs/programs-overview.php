98be84da31ecadae31bf70316910cc7de59003b0
Shiroe_11011 <ilardecharles@gmail.com>
2024-08-30 23:34:00 +0800

    programs-overview

---
+++ programs/programs-overview.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Bachelor of Science in Information Technology (BSIT)</title>
+  <link rel="stylesheet" href="../assets/css/main-style.css" />
+  <link rel="icon" href="../assets/PUPSJ-LOGO.png" type="image/icon type" />
+</head>
+
+<body>
+  <header class="header-container">
+    <?php include('../assets/static-html/header.html'); ?>
+  </header>
+
+  <section class="program">
+    <div class="header">
+      <img id="header-img" src="../assets/programs/BSIT/1.png" />
+      <div class="header-text">
+        <h id="header2">PROGRAMS</h>
+      </div>
+    </div>
+
+    <div class="content-container" >
+      <section class="pointers">
+        <h id="header" class="fade-in">CONTENTS</h>
+        <a href="#c1" class='scroll-link' class="fade-in">Program Objectives</a>
+        <a href="#c2" class='scroll-link' class="fade-in">Areas Under Survey</a>
+      </section>
+
+      <div class="main-contents">
+        <section class="overview">
+          <h id="header1">BACHELOR OF SCIENCE IN</h>
+          <h id="header" class="fade-in">ACCOUNTANCY</h>
+          <div class="thin-line"></div>
+          <h id="description" class="fade-in">
+						The <strong> Bachelor of Science in Accountancy (BSA) </strong> is a 4-year degree 
+						that prepares students for entry-level professional positions in public, private, 
+						or government accounting. Some schools may also refer to a comparable degree as a 
+						Bachelor of Accountancy. Upon graduation, students can qualify for placement in 
+						graduate or professional schools to prepare for CPA licensure.
+          </h>
+        </section>
+      </div>
+    </div>
+    <div class="content-container" >
+      <section class="pointers">
+        <h id="header" class="fade-in">CONTENTS</h>
+        <a href="#c1" class='scroll-link' class="fade-in">Program Objectives</a>
+        <a href="#c2" class='scroll-link' class="fade-in">Areas Under Survey</a>
+      </section>
+
+      <div class="main-contents">
+        <section class="overview">
+          <h id="header1">BACHELOR OF SCIENCE IN</h>
+          <h id="header" class="fade-in">ENTREPRENEURSHIP</h>
+          <div class="thin-line"></div>
+          <h id="description" class="fade-in">
+						The <strong> Bachelor of Science in Entrepreneurship (BSEnt) </strong> is a four-year 
+						course designed to provide undergraduate students an in-depth understanding and appreciation 
+						of new venture operations in small business enterprises. It aims to equip young students with 
+						an entrepreneurial spirit to realize a vibrant and developing economy in the hands of the Filipinos 
+						and develop desirable qualities of leadership, social concern, and moral values among them.
+          </h>
+        </section>
+      </div>
+    </div>
+    <div class="content-container" >
+      <section class="pointers">
+        <h id="header" class="fade-in">CONTENTS</h>
+        <a href="#c1" class='scroll-link' class="fade-in">Program Objectives</a>
+        <a href="#c2" class='scroll-link' class="fade-in">Areas Under Survey</a>
+      </section>
+
+            implementation and maintenance.
+      <div class="main-contents">
+        <section class="overview">
+          <h id="header1">BACHELOR OF SECONDARY</h>
+          <h id="header" class="fade-in">EDUCATION IN ENGLISH</h>
+          <div class="thin-line"></div>
+          <h id="description" class="fade-in">
+							The <strong>Bachelor of Secondary Education in English (BSEDEn)</strong> is a four-year course leading to the degree of Bachelor in Secondary Education,
+							major in English. It is an excellent preparation for the teaching career and for advancement in 
+							any field in which communication skills are important.
+          </h>
+        </section>
+      </div>
+    </div>
+    <div class="content-container" >
+      <section class="pointers">
+        <h id="header" class="fade-in">CONTENTS</h>
+        <a href="#c1" class='scroll-link' class="fade-in">Program Objectives</a>
+        <a href="#c2" class='scroll-link' class="fade-in">Areas Under Survey</a>
+      </section>
+
+      <div class="main-contents">
+        <section class="overview">
+          <h id="header" class="fade-in">PROGRAM OVERVIEW</h>
+          <div class="thin-line"></div>
+          <h id="description" class="fade-in">
+            The <strong> Bachelor of Science in Information Technology (BSIT) </strong>program is a four-year degree program which focuses
+            on the study of computer utilization and computer software to plan, install, customize, operate, manage,
+            administer and maintain information technology infrastructure. It likewise deals with the design and
+            development of computer-based information systems for real-world business solutions.
+            <br><br>
+            The program prepares students to become IT professionals with primary competencies in the areas of systems
+            analysis and design, applications development, database administration, network administration, and systems
+            implementation and maintenance.
+          </h>
+        </section>
+      </div>
+    </div>
+    <div class="content-container" >
+      <section class="pointers">
+        <h id="header" class="fade-in">CONTENTS</h>
+        <a href="#c1" class='scroll-link' class="fade-in">Program Objectives</a>
+        <a href="#c2" class='scroll-link' class="fade-in">Areas Under Survey</a>
+      </section>
+
+      <div class="main-contents">
+        <section class="overview">
+          <h id="header" class="fade-in">PROGRAM OVERVIEW</h>
+          <div class="thin-line"></div>
+          <h id="description" class="fade-in">
+            The <strong> Bachelor of Science in Information Technology (BSIT) </strong>program is a four-year degree program which focuses
+            on the study of computer utilization and computer software to plan, install, customize, operate, manage,
+            administer and maintain information technology infrastructure. It likewise deals with the design and
+            development of computer-based information systems for real-world business solutions.
+            <br><br>
+            The program prepares students to become IT professionals with primary competencies in the areas of systems
+            analysis and design, applications development, database administration, network administration, and systems
+            implementation and maintenance.
+          </h>
+        </section>
+      </div>
+    </div>
+    <div class="content-container" >
+      <section class="pointers">
+        <h id="header" class="fade-in">CONTENTS</h>
+        <a href="#c1" class='scroll-link' class="fade-in">Program Objectives</a>
+        <a href="#c2" class='scroll-link' class="fade-in">Areas Under Survey</a>
+      </section>
+
+      <div class="main-contents">
+        <section class="overview">
+          <h id="header" class="fade-in">PROGRAM OVERVIEW</h>
+          <div class="thin-line"></div>
+          <h id="description" class="fade-in">
+            The <strong> Bachelor of Science in Information Technology (BSIT) </strong>program is a four-year degree program which focuses
+            on the study of computer utilization and computer software to plan, install, customize, operate, manage,
+            administer and maintain information technology infrastructure. It likewise deals with the design and
+            development of computer-based information systems for real-world business solutions.
+            <br><br>
+            The program prepares students to become IT professionals with primary competencies in the areas of systems
+            analysis and design, applications development, database administration, network administration, and systems
+            implementation and maintenance.
+          </h>
+        </section>
+      </div>
+    </div>
+  </section>
+
+  <?php include('../assets/static-html/footer.html'); ?>
+
+  <script src="../main-script.js"></script>
+</body>
+
+</html>
