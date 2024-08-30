77317cfc8fcbbde977e2d6c0a8cb1df5ab4f892e
Mark Jason Fulguerinas <markjasonrfulguerinas@iskolarngbayan.pup.edu.ph>
2024-08-31 02:39:03 +0800

    upadte

---
+++ navigation/faculty.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Faculty and Staff</title>
+    <link rel="stylesheet" href="../assets/css/main-style.css" />
+    <link rel="stylesheet" href="../navigation/about-style.css" />
+    <link rel="icon" href="../assets/PUPSJ-LOGO.png" type="image/icon type" />
+</head>
+
+<body>
+    <header class="header-container">
+        <?php include('../assets/static-html/header.html'); ?>
+    </header>
+
+    <section class="area program">
+        <div class="header">
+            <img id="header-img" src="../assets/abouts/1.jpg" />
+            <div class="header-text" class="fade-in">
+
+                <h id="header2" class="fade-in">Faculty and Staff</h>
+            </div>
+        </div>
+
+
+
+        <div class="overview">
+                <p>The PUP San Juan Faculty and Staff</p>
+                <p>The realization of high quality of education envisioned by the administration of
+                    the University is strongly hinged on the qualifications and background of our faculty
+                    members and administrative staff. Meet our hard-working and passionate faculty members
+                    and administrative staff who are in the vanguard of ensuring the academic excellence of our dear University.
+                </p>
+            <div>
+
+            </div>
+
+        </div>
+    </section>
+
+    <?php include('../assets/static-html/footer.html'); ?>
+
+    <script src="../main-script.js"></script>
+</body>
+
+</html>
