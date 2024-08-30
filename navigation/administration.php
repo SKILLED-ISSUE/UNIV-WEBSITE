5616fe6629ae6647fe55980f33b81349ddbc97e4
AgustinUno <justinelloydgbautista@gmail.com>
2024-08-31 03:11:33 +0800

    admin-initial

--- navigation/administration.php
+++ navigation/administration.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Administration</title>
     <link rel="stylesheet" href="../navigation/navigation.css" />
+    <link rel="stylesheet" href="../navigation/about-style.css" />
     <link rel="stylesheet" href="../assets/css/main-style.css" />
     <link rel="icon" href="../assets/PUPSJ-LOGO.png" type="image/icon type" />
 </head>
@@ -38,81 +39,41 @@
 
         <div class="card">
             <div class="card-header">
-                The History of PUP San Juan
+            The Administration
             </div>
             <div class="card-body-history">
                 <ul>
-                    <p class="p2">
-                        Despite its proximity to the Main Campus in Sta. Mesa, the Polytechnic University of the
-                        Philippines
-                        San Juan Campus lives up to the institution’s commitment to democratize access to educational
-                        opportunities for economically challenged but deserving youths in the very heart of the National
-                        Capital Region, the historic city which served as the site of the first battle of the Philippine
-                        Revolution against Spain.
+                    <p>
+                        Governance of <strong>PUP</strong> is vested upon the <strong>Board of Regents</strong>, which
+                        exercises policy-making functions to carry out the mission and programs of the University by
+                        virtue of <strong>RA 8292</strong> granted by the Commission on Higher Education. The University
+                        is administered by an appointed President by virtue of <strong>RA 8292</strong> and is assisted
+                        by an Executive Vice President and the Vice Presidents for <strong>Academic Affairs</strong>,
+                        <strong>Student Services</strong>, <strong>Administration</strong>, <strong>Research</strong>,
+                        <strong>Extension and Development</strong>, and <strong>Finance</strong>.
                     </p>
 
                     <p>
-                        Through the relentless effort of the City Government of San Juan under the administration of
-                        then
-                        Mayor <strong>Joseph Victor “JV” G. Ejercito</strong>, who promotes the role of education in
-                        nation
-                        building, the long cherished dream of the people of San Juan to establish a public college in
-                        the
-                        city was brought to fruition. It started with the issuance of <strong>Memorandum Order No.
-                            JVE-C-13,
-                            Series of 2007</strong> which created a special committee to study and prepare the ground
-                        works
-                        for the establishment of a public tertiary academic institution.
+                        The <strong>PUP San Juan Branch</strong> is administered by a <strong>Branch Director</strong>
+                        appointed by the University President and is assisted by the Branch Officials namely
+                        <strong>Head of Student Services</strong>, <strong>Head of Admission and Registration</strong>,
+                        and <strong>Head of Academic Programs</strong>.
                     </p>
 
-                    <p>
-                        On December 13, 2007, the <strong>Sangguniang Panlungsod of San Juan</strong> issued
-                        <strong>City
-                            Resolution No. 32, Series of 2007</strong> which authorized Mayor Ejercito to sign a
-                        memorandum
-                        of agreement (MOA) with PUP, a state university established in 1904 and which continues to
-                        flourish
-                        as a stronger institution of higher learning determined to effect change in the lives of the
-                        Filipino youth.
-                    </p>
-
-                    <p>
-                        The <strong>Sangguniang Panlungsod</strong> passed on January 21, 2008 <strong>City Ordinance
-                            No. 6,
-                            Series of 2008</strong> otherwise known as “An Ordinance Establishing a PUP Campus in the
-                        City
-                        of San Juan, Metro Manila”.
-                    </p>
-
-                    <p>
-                        The signing of the MOA by Mayor Ejercito, on behalf of the City of San Juan and former
-                        University
-                        President <strong>Dr. Dante G. Guevarra</strong>, on behalf of PUP on February 20, 2008 became a
-                        landmark in San Juan’s long history of struggle for quality but accessible tertiary education.
-                    </p>
-
-                    <p>
-                        Subsequently, the <strong>PUP Board of Regents</strong> approved and ratified the MOA on March
-                        14,
-                        2008 through <strong>Board Resolution No. 613, Series of 2008</strong>. Since then and for the
-                        years
-                        to come, PUP San Juan Campus has strived and will continue to strive to become <strong>Tanglaw
-                            ng
-                            Bayan sa Dambana ng Kagitingan</strong>.
-                    </p>
-
-                    <p>
-                        Today, PUP San Juan City Campus which started as a locally funded campus is now a viable
-                        institution
-                        by virtue of <strong>Republic Act 11348 on 25 July 2019</strong> and continues to grow and
-                        develop
-                        as a higher learning institution.
-                    </p>
                 </ul>
             </div>
 
+
+            
         </div>
-        <img class="card history-banner" src="../assets/homepage-slides/3.jpg" alt="pupsj-wideshot">
+        <section class="ref-n-contents admin">
+            <div class="refs">
+                <a href="#">Branch Officials</a>
+                <a href="#">University Officials</a>
+                <a href="#">Board of Regents</a>
+
+            </div>
+
 
     </section>
 
