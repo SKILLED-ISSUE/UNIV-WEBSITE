1a0f80f1a67e2c46dc691fde5cf0cf9cadb1d110
Shiroe_11011 <ilardecharles@gmail.com>
2024-08-31 16:29:08 +0800

    WIP task force

--- navigation/local-task-force.php
+++ navigation/local-task-force.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Administration</title>
+    <title>Local Task Force</title>
     <link rel="stylesheet" href="../navigation/navigation.css" />
     <link rel="stylesheet" href="../navigation/about-style.css" />
     <link rel="stylesheet" href="../assets/css/main-style.css" />
@@ -25,51 +25,17 @@
             </div>
         </div>
 
-        <!-- <div class="line-container">
-            <div id="thin-line" class="one"></div>
+        <div class="profile-container">
+          <section class="pointers">
+            <img id="overview-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
+          </section>
+          <div class="main-contents">
+            <section class="overview-programs">
+              <div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
+              <div id="their-area">Accreditation Coordinator</div>
+            </section>
+          </div>
         </div>
-
-        <div class="line-container">
-            <div class="two" id="thin-line"></div>
-        </div> -->
-
-        <div class="card">
-            <div class="card-header">
-            The Administration
-            </div>
-            <div class="card-body-history">
-                <ul>
-                    <p>
-                        Governance of <strong>PUP</strong> is vested upon the <strong>Board of Regents</strong>, which
-                        exercises policy-making functions to carry out the mission and programs of the University by
-                        virtue of <strong>RA 8292</strong> granted by the Commission on Higher Education. The University
-                        is administered by an appointed President by virtue of <strong>RA 8292</strong> and is assisted
-                        by an Executive Vice President and the Vice Presidents for <strong>Academic Affairs</strong>,
-                        <strong>Student Services</strong>, <strong>Administration</strong>, <strong>Research</strong>,
-                        <strong>Extension and Development</strong>, and <strong>Finance</strong>.
-                    </p>
-
-                    <p>
-                        The <strong>PUP San Juan Branch</strong> is administered by a <strong>Branch Director</strong>
-                        appointed by the University President and is assisted by the Branch Officials namely
-                        <strong>Head of Student Services</strong>, <strong>Head of Admission and Registration</strong>,
-                        and <strong>Head of Academic Programs</strong>.
-                    </p>
-
-                </ul>
-            </div>
-
-
-            
-        </div>
-        <section class="ref-n-contents admin">
-            <div class="refs">
-                <a href="../navigation/branch-officials.php">Branch Officials</a>
-                <a href="../navigation/university-officials.php">University Officials</a>
-                <a href="../navigation/board-regent.php">Board of Regents</a>
-            </div>
-				</section>
-
     </section>
 
     <?php include('../assets/static-html/footer.html'); ?>
