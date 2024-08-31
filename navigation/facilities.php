726c2137d90bc0583c711c0b66217300b1b1d7c0
AgustinUno <justinelloydgbautista@gmail.com>
2024-08-31 16:47:58 +0800

    Facilities

---
+++ navigation/facilities.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Facilities</title>
+  <link rel="stylesheet" href="../assets/css/main-style.css" />
+  <link rel="stylesheet" href="../programs/overview.css" />
+  <link rel="stylesheet" href="../navigation/navigation.css" />
+  <link rel="icon" href="../assets/PUPSJ-LOGO.png" type="image/icon type" />
+</head>
+
+<body>
+  <header class="header-container">
+    <?php include('../assets/static-html/header.html'); ?>
+  </header>
+
+  <section class="program">
+    <div class="header">
+      <img id="header-img" src="../assets/abouts/1.jpg" />
+      <div class="header-text">
+        <h id="header2">Facilities</h>
+      </div>
+    </div>
+
+    <div class="content-container facilty">
+      <section class="pointers">
+        <img id="overview-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
+      </section>
+      <div class="main-contents">
+        <section class="overview-programs">
+          <div class="fade-in" id="their-name">Gymnasium</div>
+          <h id="description" class="fade-in">
+            A hall where games and physical training are held. Most of the physical activities in PUP San Juan like
+            Intramural Meet are held in the PUP San Juan gymnasium.
+          </h>
+          <div class="thin-line-break fade-in"></div>
+        </section>
+      </div>
+    </div>
+
+    <div class="content-container facilty">
+      <section class="pointers">
+        <img id="overview-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
+      </section>
+      <div class="main-contents">
+        <section class="overview-programs">
+          <div class="fade-in" id="their-name">Multi-Purpose Hall</div>
+          <h id="description" class="fade-in">
+          A hall where events sponsored by the university as well as student organizations are held.
+          </h>
+          <div class="thin-line-break fade-in"></div>
+        </section>
+      </div>
+    </div>
+
+    <div class="content-container facilty">
+      <section class="pointers">
+        <img id="overview-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
+      </section>
+      <div class="main-contents">
+        <section class="overview-programs">
+          <div class="fade-in" id="their-name">Audio-Visual Rooms</div>
+          <h id="description" class="fade-in">
+          Academic presentations, thesis defense, and the like are held in the audio-visual room. There are two audio-visual rooms in PUP San Juan
+          </h>
+          <div class="thin-line-break fade-in"></div>
+        </section>
+      </div>
+    </div>
+
+    <div class="content-container facilty">
+      <section class="pointers">
+        <img id="overview-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
+      </section>
+      <div class="main-contents">
+        <section class="overview-programs">
+          <div class="fade-in" id="their-name">Library</div>
+          <h id="description" class="fade-in">
+          In order to have a more efficient way of giving library services, PUP San Juan is now using a computerized library system that features all the services a library has. It includes: locating library resources for study and research purposes, lend books and other reading materials, and facilitates acquisition of books
+          </h>
+          <div class="thin-line-break fade-in"></div>
+        </section>
+      </div>
+    </div>
+
+    <div class="content-container facilty">
+      <section class="pointers">
+        <img id="overview-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
+      </section>
+      <div class="main-contents">
+        <section class="overview-programs">
+          <div class="fade-in" id="their-name">Computer Laboratories</div>
+          <h id="description" class="fade-in">
+          Contains computers for students’ use in laboratory quizzes, defense and other activities that has the need for the use of several computers. Students skills in computer education are practiced and enhanced in the computer laboratory. There are three computer laboratories in PUP San Juan
+          </h>
+          <div class="thin-line-break fade-in"></div>
+        </section>
+      </div>
+    </div>
+
+
+  </section>
+
+  <?php include('../assets/static-html/footer.html'); ?>
+
+  <script src="../main-script.js"></script>
+</body>
+
+</html>
