cfdd904254ad7e4fadb42ff5900b4c61925b890e
Shiroe_11011 <ilardecharles@gmail.com>
2024-09-01 14:50:44 +0800

    Technical Team nalang kulang sa local task force

--- navigation/local-task-force.php
+++ navigation/local-task-force.php
@@ -38,122 +38,142 @@
         </div>
         <div class="card">
           <div class="card-header">
-  					AREA CHAIRS
+  	    AREA CHAIRS
           </div>
-				  <div class="areas">
+	    <div class="areas">
       	  	<div class="profile-container">
-          		<section class="pointers">
-            		<img id="overview-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
-          		</section>
-          		<div class="main-contents">
-            		<section class="overview-programs">
-              		<div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
-              		<div id="their-area">Area I - VMGO</div>
+          	  <section class="pointers">
+            	    <img id="area-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
             	  </section>
-          	  </div>
+            	<div class="main-contents">
+            	  <section class="overview-programs">
+            	    <div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
+            	    <div id="their-area">Area I - VMGO</div>
+            	  </section>
+          	</div>
       	    </div>
       	  	<div class="profile-container">
-          		<section class="pointers">
-            		<img id="overview-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
-          		</section>
-          		<div class="main-contents">
-            		<section class="overview-programs">
-              		<div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
-              		<div id="their-area">Area II - Faculty</div>
+          	  <section class="pointers">
+          	    <img id="area-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
+          	  </section>
+          	<div class="main-contents">
+          	  <section class="overview-programs">
+          	    <div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
+          	    <div id="their-area">Area II - Faculty</div>
             	  </section>
-          	  </div>
+          	</div>
       	    </div>
       	  	<div class="profile-container">
-          		<section class="pointers">
-            		<img id="overview-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
-          		</section>
-          		<div class="main-contents">
-            		<section class="overview-programs">
-              		<div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
-              		<div id="their-area">Area III - Curriculum and Instruction</div>
+          	  <section class="pointers">
+          	    <img id="area-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
+          	  </section>
+          	<div class="main-contents">
+          	  <section class="overview-programs">
+          	    <div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
+          	    <div id="their-area">Area III - Curriculum and Instruction</div>
             	  </section>
-          	  </div>
+          	</div>
       	    </div>
       	  	<div class="profile-container">
-          		<section class="pointers">
-            		<img id="overview-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
-          		</section>
-          		<div class="main-contents">
-            		<section class="overview-programs">
-              		<div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
-              		<div id="their-area">Area IV - Support to Students</div>
+          	  <section class="pointers">
+          	    <img id="area-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
+          	  </section>
+          	<div class="main-contents">
+          	  <section class="overview-programs">
+          	    <div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
+          	    <div id="their-area">Area IV - Support to Students</div>
             	  </section>
-          	  </div>
+          	</div>
       	    </div>
       	  	<div class="profile-container">
-          		<section class="pointers">
-            		<img id="overview-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
-          		</section>
-          		<div class="main-contents">
-            		<section class="overview-programs">
-              		<div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
-              		<div id="their-area">Area V - Research</div>
+          	  <section class="pointers">
+          	    <img id="area-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
+          	  </section>
+          	<div class="main-contents">
+          	  <section class="overview-programs">
+          	    <div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
+          	    <div id="their-area">Area V - Research</div>
             	  </section>
-          	  </div>
+          	</div>
       	    </div>
-					</div>
-					<div class="areas">
-      	  	<div class="profile-container">
-          		<section class="pointers">
-            		<img id="overview-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
-          		</section>
-          		<div class="main-contents">
-            		<section class="overview-programs">
-              		<div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
-              		<div id="their-area">Area VI - Extension and Community Involvement</div>
-            	  </section>
-          	  </div>
+	  </div>
+	  <div class="areas">
+   	    <div class="profile-container">
+   	      <section class="pointers">
+     	        <img id="area-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
+   	      </section>
+   	      <div class="main-contents">
+     	        <section class="overview-programs">
+       	          <div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
+       	          <div id="their-area">Area VI - Extension and Community Involvement</div>
+     	        </section>
+   	      </div>
       	    </div>
-      	  	<div class="profile-container">
-          		<section class="pointers">
-            		<img id="overview-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
-          		</section>
-          		<div class="main-contents">
-            		<section class="overview-programs">
-              		<div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
-              		<div id="their-area">Area VII - Library</div>
-            	  </section>
-          	  </div>
+      	    <div class="profile-container">
+              <section class="pointers">
+                <img id="area-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
+              </section>
+              <div class="main-contents">
+            	<section class="overview-programs">
+              	  <div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
+              	  <div id="their-area">Area VII - Library</div>
+            	</section>
+              </div>
       	    </div>
-      	  	<div class="profile-container">
-          		<section class="pointers">
-            		<img id="overview-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
-          		</section>
-          		<div class="main-contents">
-            		<section class="overview-programs">
-              		<div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
-              		<div id="their-area">Area VIII - Physical Plant and Facilites</div>
-            	  </section>
-          	  </div>
+      	    <div class="profile-container">
+              <section class="pointers">
+                <img id="area-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
+              </section>
+              <div class="main-contents">
+                <section class="overview-programs">
+                  <div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
+                  <div id="their-area">Area VIII - Physical Plant and Facilites</div>
+                </section>
+              </div>
       	    </div>
-      	  	<div class="profile-container">
-          		<section class="pointers">
-            		<img id="overview-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
-          		</section>
-          		<div class="main-contents">
-            		<section class="overview-programs">
-              		<div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
-              		<div id="their-area">Area IX - Laboratory</div>
-            	  </section>
-          	  </div>
+      	    <div class="profile-container">
+              <section class="pointers">
+                <img id="area-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
+              </section>
+              <div class="main-contents">
+                <section class="overview-programs">
+                  <div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
+                  <div id="their-area">Area IX - Laboratory</div>
+                </section>
+              </div>
       	    </div>
-      	  	<div class="profile-container">
-          		<section class="pointers">
-            		<img id="overview-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
-          		</section>
-          		<div class="main-contents">
-            		<section class="overview-programs">
-              		<div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
-              		<div id="their-area">Area X - Administration</div>
-            	  </section>
-          	  </div>
+      	    <div class="profile-container">
+              <section class="pointers">
+                <img id="area-image" class="fade-in" src="https://via.placeholder.com/800x700?text=Placeholder" />
+              </section>
+              <div class="main-contents">
+                <section class="overview-programs">
+                  <div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
+                  <div id="their-area">Area X - Administration</div>
+                </section>
+              </div>
       	    </div>
-					</div>
+          </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                TECHNICAL TEAM
+            </div>
+            <div class="card-body">
+                <div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
+                <div id="their-area">Acreditation Site Leader</div>
+		<br></br>
+               <!--<p id="p2">PUP: Pambansang Politeknikong Unibersidad</p>-->
+                <div class="fade-in" id="their-name">Ma. Carina P. Corpuz</div>
+		<div id="their-area">Audtio Visual Presentation Team Leader</div>
+		<br></br>
+                <p id="p2">Members</p>
+		<div id="their-area">Name</div>
+		<div id="their-area">Name</div>
+		<div id="their-area">Name</div>
+		<div id="their-area">Name</div>
+            </div>
+
         </div>
     </section>
 
