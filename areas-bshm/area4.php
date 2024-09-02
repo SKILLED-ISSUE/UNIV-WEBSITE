f3708b791643aa73795874fac52ea5d3e5bad1ff
AgustinUno <justinelloydgbautista@gmail.com>
2024-09-02 13:15:15 +0800

    AREA 3 - HM/IT EMBEDDED UPDATE

--- areas-bshm/area4.php
+++ areas-bshm/area4.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>BSIT - Area 4</title>
+    <title>BSHM - Area 4</title>
     <link rel="stylesheet" href="../assets/css/main-style.css" />
     <link rel="icon" href="../assets/PUPSJ-LOGO.png" type="image/icon type" />
 </head>
