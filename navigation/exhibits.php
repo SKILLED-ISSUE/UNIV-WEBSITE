aa73fcbf91ae4f84cf58219c4bc0a0d4bfd3e22e
Shiroe_11011 <ilardecharles@gmail.com>
2024-09-05 00:10:29 +0800

    Exhibits php file

---
+++ navigation/exhibits.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Exhibits</title>
+    <link rel="stylesheet" href="../navigation/navigation.css" />
+    <link rel="stylesheet" href="../assets/css/main-style.css" />
+    <link rel="icon" href="../assets/PUPSJ-LOGO.png" type="image/icon type" />
+</head>
+
+<body>
+    <header class="header-container">
+        <?php include('../assets/static-html/header.html'); ?>
+    </header>
+
+    <section class="area program">
+        <div class="header">
+            <img id="header-img" src="../assets/abouts/1.jpg" />
+            <div class="header-text" class="fade-in">
+
+                <h id="header2" class="fade-in">Exhibits</h>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+            Videos
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+            Documents
+            </div>
+        </div>
+       
+    </section>
+
+    <?php include('../assets/static-html/footer.html'); ?>
+
+    <script src="../main-script.js"></script>
+</body>
+
+</html>
